6dfe7866d89a94565a9c68240aae3895906c502c
yuj2n <dbwls4595@naver.com>
2024-04-23 17:00:45 +0900

    three

--- src/f202214144/Main.java
+++ src/f202214144/Main.java
@@ -3,5 +3,6 @@ package f202214144;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("one 202214144");
+		Three.print();
 	}
 }
---
+++ src/f202214144/Three.java
@@ -0,0 +1,7 @@
+package f202214144;
+
+public class Three {
+	public static void print() {
+		System.out.println("three");
+	}
+}
